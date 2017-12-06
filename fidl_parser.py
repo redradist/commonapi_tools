0856ee90fb1e07b829ad4ad028f8c13bbe3577a7
redradist <redradist@gmail.com>
2017-12-06 23:53:35 +0200

    Removed unnessasery testing implementation

--- fidl_parser.py
+++ fidl_parser.py
@@ -4,7 +4,7 @@ import regex as re
 import itertools
 
 from jinja2 import Template
-from commonapi_types import Interface, Method, Parameter, Broadcast, Attribute
+from commonapi_types import Interface, Method, Parameter, Broadcast, Attribute, TypeCollection
 
 __comment_regex = r"(\<\*\*(?P<comment>(\<\*\*(*PRUNE)(*FAIL)|.|\n)*?)\*\*\>)?"
 __comment = re.compile(__comment_regex)
@@ -34,18 +34,29 @@ __attribute = re.compile(__attribute_regex)
 __array_regex = __comment_regex + \
                 r"\s*array\s+(?P<array_type>\w+)\s+of\s+" + __type_regex + r"\s*\n"
 __array = re.compile(__array_regex)
-__interface_version_regex = r"version\s*" + \
+__version_regex = r"version\s*" + \
                             r"{\s*major\s+(?P<major_ver>\d+)\s+minor\s+(?P<minor_ver>\d+)\s*\}\s*"
-__interface_version = re.compile(__interface_version_regex)
+__version = re.compile(__version_regex)
 __interface_regex = __comment_regex + \
                     r"\s*interface\s+(?P<name>\w+)" + \
                     r"\s*(?P<body>\{((?:[^\{\}]|(?&body))*)\})"
 __interface = re.compile(__interface_regex)
+__type_collection_regex = __comment_regex + \
+                    r"\s*typeCollection\s+(?P<name>\w+)" + \
+                    r"\s*(?P<body>\{((?:[^\{\}]|(?&body))*)\})"
+__type_collection = re.compile(__type_collection_regex)
+__import_from_regex = r"import\s+(?P<type>([\*\.\w]+))\s+from\s+\"(?P<fidl_file>([\.\w]+))\""
+__import_from = re.compile(__import_from_regex)
+__import_regex = r"import\s+model\s+\"(?P<fidl_file>([\.\w]+))\""
+__import = re.compile(__import_regex)
 __package_regex = r"package\s+(?P<name>[\.\w]+)"
 __package = re.compile(__package_regex)
 
 __test_fidl = """
 package commonapi
+
+import * from "MyBestTypes.fidl"
+import model "MyNewTypes.fidl"
 
 <** This is a test comment for HelloWorld interface **>
 interface HelloWorld {
@@ -143,7 +154,77 @@ interface HelloWorld2 {
   }
   attribute Int32 aa
 }
+
+<**
+   @description: This is a test comment for MyTypes typeCollection
+ **>
+typeCollection MyTypes
+{
+  version {major 1 minor 0}
+
+}
 """
+
+
+def parse_type_collections(fidl_file, handled_files=None):
+    """
+    This following function is parsing fidl-file
+    :param fidl_file: File to parse
+    :param handled_files: Number of handled files with types
+    :return: Raw type_collections
+    """
+    import os
+    if handled_files is None:
+        handled_files = set()
+    head, tail = os.path.split(fidl_file)
+    if tail in handled_files:
+        return
+    handled_files.add(tail)
+
+    with open(fidl_file, 'r') as file:
+        dir_path = os.path.dirname(fidl_file)
+        file_lines = file.readlines()
+        file_lines = "".join(file_lines)
+        file_lines = re.sub(r'\/\/.*\n', '', file_lines)
+        packages_name_meta = __package.finditer(file_lines)
+        for package_name_meta in packages_name_meta:
+            package_name = package_name_meta.group("name")
+        type_collections = []
+        type_collections_meta = __type_collection.finditer(file_lines)
+        if type_collections_meta:
+            for type_collection_meta in type_collections_meta:
+                type_collection_description = type_collection_meta.group("comment")
+                type_collection_name = type_collection_meta.group("name")
+                type_collection_body = type_collection_meta.group("body")
+                type_collection = TypeCollection(type_collection_name, type_collection_description)
+                type_collection.set_package_name(package_name)
+                for version_meta in __version.finditer(type_collection_body):
+                    type_collection.set_major(version_meta.group("major_ver"))
+                    type_collection.set_minor(version_meta.group("minor_ver"))
+                    break
+                type_collections.append(type_collection)
+        else:
+            print("No type_collections !!")
+
+        imports_from_meta = __import_from.finditer(file_lines)
+        for imp in imports_from_meta:
+            head, tail = os.path.split(imp.group("fidl_file"))
+            if tail in handled_files:
+                continue
+            collection_file = dir_path + "/" + imp.group("fidl_file")
+            new_type_collections = parse_type_collections(collection_file, handled_files)
+            if new_type_collections:
+                type_collections.extend(new_type_collections)
+        imports_meta = __import.finditer(file_lines)
+        for imp in imports_meta:
+            head, tail = os.path.split(imp.group("fidl_file"))
+            if tail in handled_files:
+                continue
+            collection_file = dir_path + "/" + imp.group("fidl_file")
+            new_type_collections = parse_type_collections(collection_file, handled_files)
+            if new_type_collections:
+                type_collections.extend(new_type_collections)
+        return type_collections
 
 
 def parse_interfaces(fidl_file):
@@ -160,6 +241,7 @@ def parse_interfaces(fidl_file):
         for package_name_meta in packages_name_meta:
             package_name = package_name_meta.group("name")
         print("package_name is " + str(package_name))
+        type_collections = parse_type_collections(fidl_file)
         interfaces = []
         interfaces_meta = __interface.finditer(file_lines)
         if interfaces_meta:
@@ -168,7 +250,7 @@ def parse_interfaces(fidl_file):
                 interface_name = interface_meta.group("name")
                 interface_body = interface_meta.group("body")
                 interface = Interface(interface_name, interface_description)
-                for version_meta in __interface_version.finditer(interface_body):
+                for version_meta in __version.finditer(interface_body):
                     interface.set_major(version_meta.group("major_ver"))
                     interface.set_minor(version_meta.group("minor_ver"))
                     break
@@ -178,6 +260,7 @@ def parse_interfaces(fidl_file):
                 interface.broadcasts = broadcasts
                 attributes = parse_attributes(interface_body, interface_name)
                 interface.attributes = attributes
+                interface.type_collections = type_collections
                 interface.set_package_name(package_name)
                 interfaces.append(interface)
         return interfaces
