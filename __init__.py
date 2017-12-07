762f7f43580d928ce206041ea8b7afab439ff89f
redradist <redradist@gmail.com>
2017-12-07 08:45:34 +0200

    Added usefull information in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 from commonapi_tools import fidl_parser
 from commonapi_tools import commonapi_types
 
-__version__ = '1.0'
-
 __all__ = [ 'fidl_parser', 'commonapi_types' ]
+
+__author__ = "Denis Kotov"
+__copyright__ = "Copyright 2005-2017, The CommonAPI Tools"
+__license__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Denis Kotov"
+__email__ = "redradist@gmail.com"
+__status__ = "Production"
+
